5b3d518b6cc625a5d7af5ca54ecfa76ca9e38d03
Almaz <diamond-4d@mail.ru>
2023-05-08 12:53:29 +0300

    changes

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 
+const isProd = process.env.NODE_ENV !== 'production';
+
 const devURL = 'http://localhost:4444';
 
 const prodURL = 'https://mern-blog-kosmonaf.onrender.com';
 
-export const baseURL = prodURL;
+export const baseURL = isProd ? prodURL : devURL;
 
 const instance = axios.create({
   baseURL,
